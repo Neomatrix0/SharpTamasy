01c8db07f51bdc5c10904799a065d54691a1fe90
Neomatrix0 <danimusci@yahoo.it>
2026-05-19 19:09:09 +0200

    Implemented first part of the menu

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-﻿public class Program
+﻿using System.ComponentModel.Design;
+using System.Reflection;
+
+public class Program
 {
     static void Main(string[] args)
     {
         using var db = new AppDbContext();
+
+        var service = new TaskService();
 
         var bug = new BugTask
         {
@@ -15,5 +20,47 @@
         db.SaveChanges();
 
         Console.WriteLine("Bug salvato nel database.");
+
+
+
+        bool rimani = true;
+        string? opzione = Console.ReadLine();
+        
+
+        while (rimani)
+        {
+
+            
+            Console.WriteLine("Menu SharpTamasy" );
+            Console.WriteLine("1. Aggiungi task");
+            Console.WriteLine("2. Mostra tutte le task");
+            Console.WriteLine("3. Cerca task per id");
+            Console.WriteLine("4. Elimina task");
+            Console.WriteLine("0. Esci");
+
+            switch (opzione)
+            {
+              case "1":
+              Console.WriteLine("Titolo: ");
+              string title = Console.ReadLine();
+              Console.WriteLine("Descrizione: ");
+              string description = Console.ReadLine();
+              service.AddTask(title,description);
+              break;
+
+
+             case "2":
+            var tasks = service.GetAllTasks();
+            foreach(var task in tasks)
+                    {
+                        Console.WriteLine($"{task.Id}-{task.Title}-{task.Description}-{task.CreatedAt}-{task.Status}-{task.Type}");
+                    }
+            break;
+
+            }
+
+
+
+        }
     }
 }
--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 
+
 public class TaskService
 {
     public void AddTask(string title,string description)
@@ -32,7 +33,7 @@ public class TaskService
     }
 
 
-    public void FeatureTask(string title,string description, FeaturePriority featurePriority)
+    public void AddFeatureTask(string title,string description, FeaturePriority featurePriority)
     {
         using var db = new AppDbContext();
         var featuretask = new FeatureTask
@@ -44,4 +45,89 @@ public class TaskService
         db.Tasks.Add(featuretask);
         db.SaveChanges();
     }
+
+
+    public void DeleteTask(int id)
+    {
+         using var db = new AppDbContext();
+         var task = db.Tasks.FirstOrDefault(t => t.Id == id);
+         if(task == null)
+        {
+            return;
+        }
+        db.Tasks.Remove(task);
+         db.SaveChanges();
+    }
+
+
+    public TaskItem? GetTaskById(int id)
+    {
+
+         using var db = new AppDbContext();
+         var task = db.Tasks.Find(id);
+         return task;
+            
+    }
+
+    public List<TaskItem> GetAllTasks()
+    {
+         using var db = new AppDbContext();
+         return db.Tasks.ToList();
+         
+        
+    }
+
+
+    public void UpdateTask(int id,string newTitle, string newDescription, TaskStatus newStatus)
+    {
+
+        using var db = new AppDbContext();
+          var task = db.Tasks.FirstOrDefault(t => t.Id == id);
+         if(task == null)
+        {
+            return;
+        }
+        task.Title = newTitle;
+        task.Description = newDescription;
+        task.Status  = newStatus;
+        db.SaveChanges();
+
+       
+
+    }
+
+
+     public void UpdateFeaturePriority(int id,FeaturePriority newPriority)
+    {
+
+        using var db = new AppDbContext();
+          var task = db.Tasks.FirstOrDefault(t => t.Id == id);
+         if(task is not FeatureTask feature)
+        {
+            return;
+        }
+      
+        feature.Priority = newPriority;
+        
+        db.SaveChanges();    
+
+    }
+
+
+    public void UpdateBugSeverity(int id, BugSeverity newSeverity)
+    {
+        using var db = new AppDbContext();
+        var task =  db.Tasks.FirstOrDefault(t => t.Id == id);
+
+         if (task is not BugTask bug)
+    {
+        return;
+    }
+        
+        bug.Severity = newSeverity;
+        
+        db.SaveChanges();
+    }
 }
+
+
